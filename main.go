6fec985d7a2ef6cfef0a932ccb86823fafae84aa
Anideep-Kalia <anideepkalia@gmail.com>
2024-12-03 23:00:58 +0530

    understandng and modularising

--- main.go
+++ main.go
@@ -6,91 +6,75 @@ import (
 	"log"
 	"os"
 
-	"github.com/gocolly/colly" // package for webscraping
+	"github.com/gocolly/colly"
 )
 
-type Stock struct{
-company, price, change string
+type Stock struct {
+	company, price, change string
 }
 
 var ticker = []string{
-	"MSFT",
-	"IBM",
-	"GE",
-	"UNP",
-	"COST",
-	"MCD",
-	"V",
-	"WMT",
-	"DIS",
-	"MMM",
-	"INTC",
-	"AXP",
-	"AAPL",
-	"BA",
-	"CSCO",
-	"GS",
-	"JPM",
-	"CRM",
-	"VZ",
-
+	"MSFT", "IBM", "AAPL", "GOOG", "AMZN",
 }
 
-func main(){
-
-	stocks := []Stock{}
-
-	// interface for webscraping
-	c := colly.NewCollector()
-
-	// colly specific functions
+// Function to set up Colly callbacks
+// below functions can't be called because these are callback function and will execute after automatically when event is triggered
+func setupCallbacks(c *colly.Collector, stocks *[]Stock) {
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting:", r.URL)
 	})
-	c.OnError(func(_ *colly.Response, err error) { 
-		log.Println("Something went wrong: ", err) 
-	}) 
-
-	c.OnHTML("div#quote-header-info", func(e *colly.HTMLElement) {
-		
-		stock := Stock{}
-
-		stock.company = e.ChildText("h1")
-		fmt.Println("Company:", stock.company)
-		stock.price = e.ChildText("fin-streamer[data-field='regularMarketPrice']")
-		fmt.Println("Price:", stock.price)
-		stock.change = e.ChildText("fin-streamer[data-field='regularMarketChangePercent']")
-		fmt.Println("Change:", stock.change)
-
-		stocks = append(stocks, stock)
+	c.OnError(func(r* colly.Response,err error){
+		log.Println("Error occurred:", err)
 	})
-	c.Wait()
+	c.OnHTML("div#quote-header-info", func(e *colly.HTMLElement) {
+		stock := Stock{
+			company: e.ChildText("h1"),
+			price:   e.ChildText("fin-streamer[data-field='regularMarketPrice']"),
+			change:  e.ChildText("fin-streamer[data-field='regularMarketChangePercent']"),
+		}
+	
+		fmt.Printf("Scraped: %+v\n", stock)
+		*stocks = append(*stocks, stock)
+	})
+}
 
+func main() {
+	stocks := []Stock{}
+	c := colly.NewCollector()
+
+	// Set up callbacks
+	setupCallbacks(c, &stocks)
+
+	// Visit each ticker URL
 	for _, t := range ticker {
-	c.Visit("https://finance.yahoo.com/quote/" + t + "/")
+		c.Visit("https://finance.yahoo.com/quote/" + t + "/")
 	}
 
-	fmt.Println(stocks)
+	c.Wait() // Ensure all scraping tasks are completed
 
-	file, err := os.Create("stocks.csv") 
-	if err != nil { 
-		log.Fatalln("Failed to create output CSV file", err) 
-	} 
-	defer file.Close() 
+	// Save the results to a CSV file
+	saveToCSV(stocks)
+}
+
+
+// Function to save the scraped data to a CSV file
+func saveToCSV(stocks []Stock) {
+	file, err := os.Create("stocks.csv")
+	if err != nil {
+		log.Fatalln("Failed to create CSV file:", err)
+	}
+	defer file.Close()
+
 	writer := csv.NewWriter(file)
-	headers := []string{
-		"company", 
-		"price", 
-		"change", 
-	} 
-	writer.Write(headers) 
-	for _, stock := range stocks { 
-		record := []string{ 
-			stock.company,
-			stock.price,
-			stock.change,
-		} 
-		writer.Write(record) 
-		}
-		defer writer.Flush();
+	defer writer.Flush()
+
+	// Write the header
+	writer.Write([]string{"Company", "Price", "Change"})
+
+	// Write stock data
+	for _, stock := range stocks {
+		writer.Write([]string{stock.company, stock.price, stock.change})
+	}
+
+	fmt.Println("Data saved to stocks.csv")
 }
